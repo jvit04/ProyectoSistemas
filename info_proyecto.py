64366c5b2e07f897f5f6e929fd4659feaf0b515f
josehidalgoc-cpu <josehidalgoc@uees.edu.ec>
2026-03-04 01:29:12 +0000

    Adjuntando archivo info_proyecto.p

---
+++ info_proyecto.py
@@ -0,0 +1,8 @@
+def descripcion_proyecto():
+
+    print("PROYECTO: ESTACIONAMIENTO INTELIGENTE Descripción:")
+    print("Sistema que controla el ingreso y salida de vehículos en un estacionamiento " \
+    "con cupos limitados, registrando la ocupación y los tiempos de permanencia de cada vehículo.")
+
+if __name__ == "__main__":
+    descripcion_proyecto()
